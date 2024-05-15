c9a4efff97282ea2b1cbcaa2433adfd3a956c703
Rohini Singh <rohinisingh2201@gmail.com>
2024-05-15 19:05:46 +0530

    Create FindDuplicateNum.java

---
+++ FindDuplicateNum.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/find-the-duplicate-number/description/
+class Solution {
+    //USING CYCLIC SORT
+    public int findDuplicate(int[] arr) {
+        int i = 0;
+        while(i < arr.length) {
+
+            if(arr[i] != i + 1) {
+                int correct = arr[i] - 1;
+                if(arr[i] != arr[correct]) {
+                    swap(arr, i, correct);
+                } else {
+                    return arr[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
